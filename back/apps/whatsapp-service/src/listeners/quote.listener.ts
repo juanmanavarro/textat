f4050fb37090990e3ce0b2e129a0c3b257cbf4d2
juanma <juanaco1.p@gmail.com>
2023-04-01 10:05:53 +0200

    schedule message

--- back/apps/whatsapp-service/src/listeners/quote.listener.ts
+++ back/apps/whatsapp-service/src/listeners/quote.listener.ts
@@ -1,13 +1,13 @@
-import { PostService } from "@domain/post/post.service";
 import { Injectable } from "@nestjs/common";
 import { TransportService } from "@transport/transport";
 import { ReminderHandler } from "../handlers/reminder.handler";
 import { ParserService } from "../services/parser.service";
+import { MessageService } from "@domain/message/message.service";
 
 @Injectable()
 export class QuoteListener {
   constructor(
-    private readonly postService: PostService,
+    private readonly messageService: MessageService,
     private readonly transportService: TransportService,
     private readonly reminderHandler: ReminderHandler,
   ) {}
@@ -15,17 +15,15 @@ export class QuoteListener {
   async handle(user, message) {
     if ( !message.context ) return false;
 
-    const quoted = await this.postService
+    const quoted = await this.messageService
       .findOne({ whatsapp_id: message.context.id });
     if ( !quoted ) return false;
 
     const {
-      comment,
       schedule
     } = ParserService.extractEntities(message.text.body);
 
     if ( schedule ) await this.reminderHandler.handle(user, quoted, schedule);
-    if ( comment ) quoted.comments.push(comment);
 
     await quoted.save();
 
