7263d8bb6c04c3e128224608c7ee3ede8aa79127
juanma <juanaco1.p@gmail.com>
2023-04-01 09:28:21 +0200

    remove api

--- back/apps/api/src/app.module.ts
+++ back/apps/api/src/app.module.ts
@@ -4,14 +4,8 @@ import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 import { SharedModule } from '@shared';
 import configuration from '../../../config';
-import { AuthController } from './controllers/auth.controller';
-import { HealthCheckController } from './controllers/health-check.controller';
 import { TransportModule } from '@transport/transport';
-import { PostController } from './controllers/post.controller';
-import { CategoryController } from './controllers/category.controller';
-import { TagController } from './controllers/tag.controller';
 import { SubscriptionModule } from './subscription/subscription.module';
-import { DashboardModule } from './dashboard/dashboard.module';
 
 @Module({
   imports: [
@@ -24,14 +18,6 @@ import { DashboardModule } from './dashboard/dashboard.module';
     SharedModule,
     TransportModule,
     SubscriptionModule,
-    DashboardModule,
-  ],
-  controllers: [
-    AuthController,
-    PostController,
-    HealthCheckController,
-    CategoryController,
-    TagController,
   ],
 })
 export class AppModule {}
