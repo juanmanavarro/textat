e541df3a0f35ec93376fc02790c0d13eb5add9cd
juanma <juanaco1.p@gmail.com>
2023-04-02 12:39:52 +0200

    schedule directly

--- back/apps/whatsapp-service/src/listeners/post.listener.ts
+++ back/apps/whatsapp-service/src/listeners/post.listener.ts
@@ -3,6 +3,7 @@ import { TransportService } from '@transport/transport';
 import { PostMapperService } from '../services/post-mapper.service';
 import { MessageService } from '@domain/message/message.service';
 import { ParserService } from '../services/parser.service';
+import { DateService } from '@shared/services/date.service';
 
 @Injectable()
 export class PostListener {
@@ -17,9 +18,18 @@ export class PostListener {
     const { temp, rest } = await this.parserService.parse(message.text.body);
     message.text.body = rest;
 
+    let scheduled_at = null;
+    if ( temp ) {
+      scheduled_at = DateService.parse(temp, user.timezone);
+    }
+
     const maps = await this.postMapperService.map(user, message);
     for (const mapped of maps) {
-      const post = await this.messageService.firstOrCreate({ ...mapped, schedule: temp || null });
+      const post = await this.messageService.firstOrCreate({
+        ...mapped,
+        schedule: temp || null,
+        scheduled_at: scheduled_at?.toDate(),
+      });
       if ( !post['is_new'] ) continue;
 
       this.transportService.send('post:created', {
