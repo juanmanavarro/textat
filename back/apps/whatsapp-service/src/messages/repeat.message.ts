7bbbfc82f46d884226de8fb756c70f05b3ec3b6a
juanma <juanaco1.p@gmail.com>
2023-04-04 11:35:00 +0200

    redo help repaet message

--- back/apps/whatsapp-service/src/messages/repeat.message.ts
+++ back/apps/whatsapp-service/src/messages/repeat.message.ts
@@ -1,9 +1,9 @@
 import { FormatService } from "../services/format.service";
 
 export const repeatMessage = [
-  `${FormatService.bold('/repeat y')} to be repeated every year`,
-  `${FormatService.bold('/repeat mo')} to be repeated every month`,
-  `${FormatService.bold('/repeat d')} to be repeated every day`,
-  `${FormatService.bold('/repeat h')} to be repeated every hour`,
   `${FormatService.bold('/repeat m')} to be repeated every minute`,
+  `${FormatService.bold('/repeat h')} to be repeated every hour`,
+  `${FormatService.bold('/repeat d')} to be repeated every day`,
+  `${FormatService.bold('/repeat mo')} to be repeated every month`,
+  `${FormatService.bold('/repeat y')} to be repeated every year`,
 ].join('\n');
