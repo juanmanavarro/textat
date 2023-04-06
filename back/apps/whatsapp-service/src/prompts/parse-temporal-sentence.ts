659e6fbf5727dced1411fd987b9275490c4b4804
juanma <juanaco1.p@gmail.com>
2023-04-06 11:24:14 +0200

    translate prompt

--- back/apps/whatsapp-service/src/prompts/parse-temporal-sentence.ts
+++ back/apps/whatsapp-service/src/prompts/parse-temporal-sentence.ts
@@ -1,13 +1,13 @@
 export default (sentence) => {
   return `Vamos a parsear frases. La respuesta debe de ser un JSON valido con las siguientes propiedades:
-    temp: la parte de la frase que se refiere al tiempo, es decir, la indicacion temporal de la frase
+    temp: la parte de la frase que se refiere al tiempo, es decir, la indicacion temporal de la frase. Traducela a ingles
     rest: la parte restante de la frase. Lo que queda al extraer la indicacion temporal de la frase original
     frase: "tengo dentista mañana a las 10"
-    respuesta: { "temp": "mañana a las 10", "rest": "tengo dentista" }
+    respuesta: { "temp": "tomorrow at 10 a.m.", "rest": "tengo dentista" }
     frase: "ayer cene con mi amigo por la noche"
-    respuesta: { "temp": "ayer por la noche", "rest": "cene con mi amigo" }
+    respuesta: { "temp": "last night", "rest": "cene con mi amigo" }
     frase: "1 en un minuto"
-    respuesta: { "temp": "en un minuto", "rest": "1" }
+    respuesta: { "temp": "in a minute", "rest": "1" }
     frase: "${sentence}"
     respuesta:`;
 }
